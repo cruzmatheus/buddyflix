d0699c9d212e59a291b6a01665bf406208303e78
Matheus Cruz <contato.mscruz@gmail.com>
2019-05-08 23:11:27 +0100

    Building horizontal movie's list

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -20,15 +20,63 @@ class _HomePage extends State<HomePage> {
       builder: (BuildContext context, HomeState homeState) {
         if (homeState is InitialHomeState) {
           _homeBloc.dispatch(FetchGenres());
-          return Center(child: Text("InitialHomeState"));
+          return Center(
+              child: CircularProgressIndicator(backgroundColor: Colors.red));
         } else if (homeState is GenresFetchedState) {
           return ListView.builder(
               itemBuilder: (BuildContext context, int index) {
-                return Text(homeState.genres[index].name);
+                // return GenreWidget(name: homeState.genres[index].name);
+                return buildMovieHorizontalList(homeState.genres[index].name);
               },
               itemCount: homeState.genres.length);
         }
       },
     );
+  }
+
+  Widget buildMovieHorizontalList(String genreName) {
+    return Container(
+      padding: EdgeInsets.only(left: 5, right: 5),
+      height: 240,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[Text(genreName)],
+            ),
+          ),
+          Container(
+              child: Container(
+            height: 200,
+            child: ListView(
+              padding: EdgeInsets.all(3),
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(left: 5, right: 10),
+                  width: 130,
+                  child: Image(
+                    image: AssetImage("assets/1.jpg"),
+                    fit: BoxFit.fitHeight,
+                  ),
+                )
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+class GenreWidget extends StatelessWidget {
+  final String name;
+
+  const GenreWidget({Key key, this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(title: Text(name));
   }
 }
